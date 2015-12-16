96efa8efb83de8ab312f643bd72cff00fe4bb0f4
Steve Raden <steve.raden@rvibe.com>
2015-12-15 22:47:57 -0600

    cleanup

--- get_logs.rb
+++ get_logs.rb
@@ -5,30 +5,29 @@ require 'json'
 
 
 def get_logs(domain)
-  key = ENV["MAILGUN_KEY"]  
+  key = ENV["MAILGUN_KEY"]
   response = RestClient.get "https://api:#{key}"\
-  "@api.mailgun.net/v3/#{domain}/log", :params => {    
+  "@api.mailgun.net/v3/#{domain}/log", :params => {
     :limit => 200
   }
-  JSON.parse(response)  
+  JSON.parse(response)
 end
 
-def process_logs logs
+def process_logs(logs)
   report = Array.new
   report << logs["items"].first.keys.join("|")
   logs["items"].each do |item|
-    report << item.values.join("|")        
+    report << item.values.join("|")
   end
   return report
 end
 
 domain = ARGV[0]
-byebug
 report = process_logs(get_logs(domain))
 File.open("mailgun_report.txt", 'w') do |file|
   report.each do |line|
     file.puts(line)
-  end  
-end 
+  end
+end
 
 
